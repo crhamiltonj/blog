79c0f9e626ccaa0aa837dbba2571dc8ef99d6e88
Charles Hamilton <crhamiltonj@gmail.com>
2019-02-17 20:45:31 -0500

    Completed blog app.

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,5 +1,13 @@
+import _ from 'lodash';
 import jsonPlaceholder from "../apis/jsonPlaceholder";
 
+export const fetchPostsAndUsers = () => async (dispatch, getState) => {
+    await dispatch(fetchPosts());
+
+    const userIds =_.uniq(_.map(getState().posts, 'userId'));
+    userIds.forEach(id => dispatch(fetchUser(id)));
+
+};
 export const fetchPosts = () => async dispatch => {
         const response = await jsonPlaceholder.get('/posts');
 
@@ -11,4 +19,5 @@ export const fetchUser = (id) => async dispatch => {
 
         dispatch({type: 'FETCH_USER', payload: response.data });
 };
+
 
